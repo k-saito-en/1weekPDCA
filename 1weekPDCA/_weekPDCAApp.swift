a3ea3c82ae2cb27a43844bd1a04f7b31b2d4b10f
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-01-30 15:28:43 +0900

    :gear:SwiftUIプロジェクトの作成 👀 プロジェクトのコピーを作成して移動させたので「ビルドターゲット」などの設定に不備がないか要確認

---
+++ 1weekPDCA/_weekPDCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  _weekPDCAApp.swift
+//  1weekPDCA
+//
+//  Created by 齊藤虎太郎 on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct _weekPDCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
