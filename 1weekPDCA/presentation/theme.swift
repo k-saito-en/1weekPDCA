bcd363dab346e32e2a7d53217c8b59b0c87fb1d2
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-02-07 17:38:56 +0900

    :gear: issue #3 テキストスタイルを指定 - 構造体プロパティの関数として実装し、View実装時に都度引数 color を指定できる実装にした。後々の「プログレスサークルの色を動的に変更させる」で効果を発揮しそう！

--- 1weekPDCA/presentation/theme.swift
+++ 1weekPDCA/presentation/theme.swift
@@ -6,5 +6,38 @@
 //
 
 import Foundation
+import SwiftUI
+
+// テキストサイズ
+struct TextSize {
+    static let titleTextSize: CGFloat = 18
+    static let bodyTextSize: CGFloat = 12
+    static let progressCircleTextSize: CGFloat = 40
+}
 
 
+// テキストスタイル
+struct TextStyles {
+    
+    // タイトルテキストの指定（Colorは引数で都度指定）
+    static func titleText(color: Color) -> Text {
+        return Text(verbatim: "")
+            .font(Font.system(size: TextSize.titleTextSize, weight: .medium, design: .default))
+            .foregroundColor(color)
+    }
+    
+    // 本文テキストの指定（Colorは引数で都度指定）
+    static func bodyText(color: Color) -> Text {
+        return Text(verbatim: "")
+            .font(Font.system(size: TextSize.bodyTextSize, weight: .regular, design: .default))
+            .foregroundColor(color)
+    }
+    
+    // プログレスサークルテキストの指定（Colorは引数で都度指定）
+    // 達成率によって色を変化させる予定なのでこの色変更可能な実装の方が都合がよさそう
+    static func progressCircleText(color: Color) -> Text {
+        return Text(verbatim: "")
+            .font(Font.system(size: TextSize.progressCircleTextSize, weight: .bold, design: .default))
+            .foregroundColor(color)
+    }
+}
