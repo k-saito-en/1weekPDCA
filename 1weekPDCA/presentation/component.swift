c560d7ef5cb5bd9bfa5dc93465877c0473c1a5d4
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-02-09 13:16:35 +0900

    :sparkles: issue #3 Bottom barの実装、アイコン設定、アイコンの色設定

--- 1weekPDCA/presentation/component.swift
+++ 1weekPDCA/presentation/component.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
+
+// ボトムバーの実装
+struct BottomBar: View {
+    var body: some View {
+        TabView {
+         // Act画面
+         Text("ActTabView()")
+            .tabItem {
+                Image(systemName: "graduationcap.fill")
+                    .renderingMode(.template)
+                    .foregroundColor(Color("UIColorGray"))
+            }
+         // Plan・Do画面
+         Text("PlanDoTabView()")
+            .tabItem {
+                Image(systemName: "square.fill")
+                    .renderingMode(.template)
+                    .foregroundColor(Color("UIColorGray"))
+            }
+         // Check画面
+         Text("CheckTabView()")
+            .tabItem {
+                Image(systemName: "checkmark.circle.fill")
+                    .renderingMode(.template)
+                    .foregroundColor(Color("UIColorGray"))
+            }
+        }
+    }
+}
