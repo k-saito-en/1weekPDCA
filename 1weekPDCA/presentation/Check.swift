6016930dae6c96958e75e3400b557f183a9e853a
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-09-07 19:46:44 +0900

    :art: #7 ボトムバー表示に合わせて日付カードの高さを調節 / ボトムバーのチェックタブに`CheckView`をリンク

--- 1weekPDCA/presentation/Check.swift
+++ 1weekPDCA/presentation/Check.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Check: View {
+struct CheckView: View {
     var body: some View {
         ZStack {
             
@@ -15,30 +15,35 @@ struct Check: View {
             
             ScrollView {
                 
-                ZStack {
+                VStack {
                     
-                    Rectangle()
-                        .fill(Color.cardColorGray)
-                        .cornerRadius(25)
-                        .frame(height: 60)
-                        .padding(.horizontal, 20)
-                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 5)
-                   
-                    HStack {
+                    Color.clear.frame(width:200, height: 20)
+                    
+                    ZStack {
                         
-                        Text("12 / 24")
-                            .textStyle(for: .title, color: Color.uiColorGray)
-//                            .padding(.leading, 40)
-                        
-                        Color.clear.frame(width:200, height: 40)
-                        
-                        Text("Sun")
-                            .textStyle(for: .title, color: Color.uiColorGray)
+                        Rectangle()
+                            .fill(Color.cardColorGray)
+                            .cornerRadius(25)
+                            .frame(height: 60)
+                            .padding(.horizontal, 20)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 5)
+                       
+                        HStack {
+                            
+                            Text("12 / 24")
+                                .textStyle(for: .title, color: Color.uiColorGray)
+    //                            .padding(.leading, 40)
+                            
+                            Color.clear.frame(width:200, height: 40)
+                            
+                            Text("Sun")
+                                .textStyle(for: .title, color: Color.uiColorGray)
+                            
+                        }
+                        .frame(width: .infinity, height: 60)
+                        .padding(.horizontal, 30)
                         
                     }
-                    .frame(width: .infinity, height: 60)
-                    .padding(.horizontal, 30)
-                    
                 }
             }
         }
@@ -47,6 +52,6 @@ struct Check: View {
 
 struct Check_Previews: PreviewProvider {
     static var previews: some View {
-        Check()
+        CheckView()
     }
 }
